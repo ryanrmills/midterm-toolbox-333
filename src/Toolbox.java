0021d7c500aec927e1eaecaf3740b293f58ae8b3
ryanrmills <ryanroymills@gmail.com>
2026-01-31 14:39:26 -0800

    completed some problems

--- src/Toolbox.java
+++ src/Toolbox.java
@@ -14,7 +14,15 @@ public class Toolbox {
     if (head == null) {
       throw new IllegalArgumentException("Head cannot be null.");
     }
-    return -1; 
+
+    SingleNode current = head;
+    int count = 0;
+    while (current != null){
+      count++;
+      current = current.next;
+    }
+
+    return count;
   }
 
   /**
@@ -28,7 +36,13 @@ public class Toolbox {
     if (head == null) {
       throw new IllegalArgumentException("Head cannot be null.");
     }
-    return null; 
+    
+    SingleNode current = head;
+    while (current.next != null){
+      current = current.next;
+    }
+
+    return current;
   }
 
   /**
@@ -42,7 +56,13 @@ public class Toolbox {
     if (tail == null) {
       throw new IllegalArgumentException("Tail cannot be null.");
     }
-    return null; 
+
+    DoubleNode current = tail;
+    while (current.prev != null){
+      current = current.prev;
+    }
+
+    return current;
   }
 
   /**
